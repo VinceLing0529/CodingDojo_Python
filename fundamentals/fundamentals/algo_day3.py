ce3bd15b997d88158e1e31036e19910953f170ef
VinceLing0529 <ldkxnb@gmail.com>
2021-05-05 12:55:04 -0700

    algo

---
+++ fundamentals/fundamentals/algo_day3.py
@@ -0,0 +1,10 @@
+str1 = "a3b2c1d3";
+expected1 = "aaabbcddd";
+str2 = "a3b2c12d10";
+expected2 = "aaabbccccccccccccdddddddddd";
+
+def decode(str):
+    for i in str:
+        try int(i):
+            while()
+
