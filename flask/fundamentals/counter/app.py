cda5dcd72492e54d7d9199b844d0ad5196f5d45f
VinceLing0529 <ldkxnb@gmail.com>
2021-05-07 12:38:33 -0700

    counter

--- flask/fundamentals/counter/app.py
+++ flask/fundamentals/counter/app.py
@@ -3,19 +3,33 @@ app = Flask(__name__)
 app.secret_key = 'keep it secret, keep it safe'
 # our index route will handle rendering our form
 
-@app.route('/')
+@app.route('/',methods=['GET','POST'])
 
 def index():
+    if "actual" not in session:
+        session["actual"]=1
+    else:
+        session["actual"]+=1
+
     if "count" not in session:
-        session["count"]=1
+        session["count"]==1        
     else:
         session["count"]+=1
+
+    if request.method == 'POST':
+        if request.form["button"]=="Add2":
+            session["count"]+=1
+        if request.form["button"]=="Reset":
+            session.pop('count')
+            session["count"]=1
+
+
     return render_template("index.html")
 
 @app.route('/destroy_session')
 
 def destry():
-    session.clear()	
+    session.pop('count')    
     return redirect("/")
 
 if __name__ == "__main__":
