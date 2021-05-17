8bded015aede049651d15b46cdfc16510799646a
VinceLing0529 <ldkxnb@gmail.com>
2021-05-17 15:42:38 -0700

    dojo_survey

--- flask_mysql/algo1.js
+++ flask_mysql/algo1.js
@@ -63,3 +63,96 @@ function invertObj(obj) {
     return new_map
 }
 console.log(invertObj(obj1))
+
+/* 
+  Balance Index
+  Here, a balance point is ON an index, not between indices.
+  Return the balance index where sums are equal on either side
+  (exclude its own value).
+  
+  Return -1 if none exist.
+  
+*/
+
+const nums1 = [-2, 5, 7, 0, 3];
+const expected1 = 2;
+
+const nums2 = [9, 9];
+const expected2 = -1;
+
+const nums1 = [-2, 5, 7, 0, 3,0];
+
+
+/**
+ * Finds the balance index in the given array where the sum to the left of the
+ *    index is equal to the sum to the right of the index.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {number} The balance index or -1 if there is none.
+ */
+function balanceIndex(nums) {
+  for(i=1;i<nums.length;i++){
+    var left = 0;
+    var right = 0;
+    for(j=0;j<i;j++){
+      left+=nums[j];
+    }
+    for(z=i+1;z<nums.length;z++){
+      right +=nums[z];
+    }
+    if(left == right){
+      return i;
+    }
+  } 
+  return -1
+}
+balanceIndex(nums1)
+
+module.exports = { balanceIndex };
+
+// **********************************************************************
+// **********************************************************************
+// **********************************************************************
+
+
+/* 
+  Balance Point
+  Write a function that returns whether the given
+  array has a balance point BETWEEN indices, 
+  where one side’s sum is equal to the other’s. 
+*/
+
+const nums1 = [1, 2, 3, 4, 10];
+const expected1 = true;
+// Explanation: between indices 3 & 4
+
+const nums2 = [1, 2, 4, 2, 1];
+const expected2 = false;
+
+/**
+ * Determines if there is a balance point BETWEEN indexes where the sum of the
+ *    left side is equal to the sum of the right side of the given array.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {Array<number>} nums
+ * @returns {boolean} Indicates if a balance point exists.
+ */
+function balancePoint(nums) {
+  for(i=0;i<nums.length;i++){
+    var left =0
+    var right =0
+    for(j=0;j<i;j++){
+      left += nums[j];
+    }
+    for(z=i;z<nums.length;z++){
+      right += nums[z];
+    }
+    if(left == right){
+      return true;
+    }
+  }
+  return false
+}
+
+module.exports = { balancePoint };
